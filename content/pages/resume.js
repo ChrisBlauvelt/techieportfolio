751749b4631642583245485e9314b0a4eb7855c0
Chris Blauvelt <admin@blauv.net>
2024-10-09 00:03:18 -0400

    Update resume.js
    react resume

--- content/pages/resume.js
+++ content/pages/resume.js
@@ -1,27 +1,11 @@
-import React, { useEffect } from 'react';
-import Head from 'next/head';
+import React from 'react';
+import PDFViewer from '../components/PDFViewer';
 
-const ResumePage = () => {
-  useEffect(() => {
-    // You can add analytics or other side effects here
-    console.log('Resume page viewed');
-  }, []);
-
+const ResumePage: React.FC = () => {
   return (
-    <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
-      <Head>
-        <title>My Resume</title>
-        <meta name="Chris Blauvelt's Resume" content="View my professional resume" />
-      </Head>
-      <h1 style={{ textAlign: 'center' }}>My Resume</h1>
-      <iframe 
-        src="/resume.pdf"
-        style={{
-          width: '100%',
-          height: '100%',
-          border: 'none'
-        }}
-      />
+    <div>
+      <h1>My Resume</h1>
+      <PDFViewer />
     </div>
   );
 };
